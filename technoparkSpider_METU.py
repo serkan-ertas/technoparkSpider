011b934974f5dc6fe30a3c35e83e807eae8a8094
Serkan <serkan.ertas.professional@gmail.com>
2024-07-18 18:56:53 +0300

    Update technoparkSpider_METU.py

--- technoparkSpider_METU.py
+++ technoparkSpider_METU.py
@@ -2,34 +2,12 @@
 ###
 ### scrapy runspider technoparkSpider_METU.py -O firm_data_METU.json
 
-
 import scrapy
-import json
-import re
-from urllib.parse import urlparse
-import os
+from utils.utils import *
 
 
-def is_valid_url(url):
-    pattern = re.compile(r"^https?://(?:www\.)?\w+\.\w+$")
-    return bool(pattern.match(url))
-
-
-def extract_domain(url):
-    parsed_url = urlparse(url).netloc
-
-    if parsed_url.startswith("www."):
-        parsed_url = parsed_url[4:]
-
-    return parsed_url
-
-
-def clearUrls(urls):
-    return [extract_domain(url) for url in urls if is_valid_url(url)]
-
-
-class TechnoparkSpiderMETU(scrapy.Spider):
-    name = "technoparkSpider"
+class METUSpider(scrapy.Spider):
+    name = "METUSpider"
     start_urls = ["https://odtuteknokent.com.tr/tr/firmalar/tum-firmalar.php"]
 
     def parse(self, response):
@@ -40,6 +18,6 @@ class TechnoparkSpiderMETU(scrapy.Spider):
         urls = clearUrls(urls)
 
         for url in urls:
-            yield{
-                'url':url
+            yield {
+                'url': url
             }
